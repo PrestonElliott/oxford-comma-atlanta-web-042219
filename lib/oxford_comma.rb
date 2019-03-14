5356e77fa51766e5ec2a945699928ef52e1ef06b
Preston Elliott <preston.elliott24@gmail.com>
2019-03-14 20:14:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,17 @@
 def oxford_comma(array)
-  if array.length == 2
-    return array.join(" and ")
-  elsif array.length >= 3
-    return array(0..1).join(", ") && array(-1).join(" and ")
-  else
-    return array(0..-2).join(", ") && array(-1).join(" and ")
-  end
+  return array.join(" and ")
 end
+
+
+# def oxford_comma(array)
+#   if array.length == 2
+#     return array.join(" and ")
+#   elsif array.length >= 3
+#     return array(0..1).join(", ") && array(-1).join(" and ")
+#   else
+#     return array(0..-2).join(", ") && array(-1).join(" and ")
+#   end
+# end
 
 
 # passes first two test 
