079dc63ec0d908cc0da8cb05f6907cc20d4c0dab
Preston Elliott <preston.elliott24@gmail.com>
2019-03-14 20:06:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -13,3 +13,14 @@ end
 # def oxford_comma(array)
 #   return array.join(" and ")
 # end
+
+# range example from prime numbers lab
+# def prime?(num)
+#     return false if num < 2
+#     (2..num - 1).each do |x|
+#         if (num % x) == 0
+#             return false
+#         end
+#     end
+#     true
+# end
