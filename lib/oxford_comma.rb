5485b8a53cc4ddbe69d2b47c6021b75e98874333
Preston Elliott <preston.elliott24@gmail.com>
2019-03-14 20:20:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,14 @@
 def oxford_comma(array)
-  if array.length != 0 && array.length > 2
-    last element = array[-1]
+  if array.length == 1 
+    array.join(", ")
+  elsif array.length == 2 
+    array.join(" and ")
+  elsif array.length != 0 && array.length > 2
+    last_element = array[-1]
     array.pop
     new_array = array.join(", ")
-    
+    new_array << " and " + last_element
+  end
 end
 
 
